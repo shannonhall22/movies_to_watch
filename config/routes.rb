2488eb1d0ea18228cec6ef5887d52d1f428d894d
shannonhall22 <shannonhall22@gmail.com>
2015-04-29 20:36:49 -0500

    controller and action added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
 
+  #Create
+  get( ,:controller => "movies", :action => "new_form")
+  post( ,:controller => "movies", :action => "create_row")
+
+  #Read
+  get( ,:controller => "movies", :action => "show")
+  get( ,:controller => "movies", :action => "index")
+
+  #Edit
+  get( ,:controller => "movies", :action => "edit_form")
+  patch( ,:controller => "movies", :action => "update_row")
+
+  #Destroy
+  delete( ,:controller => "movies", :action => "destroy")
+
 end
